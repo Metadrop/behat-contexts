1bc4b1e0d357ccb3322aa8a69af22c2e7a11e381
Ricardo Sanz <ricardo@metadrop.net>
2016-04-27 17:27:43 +0200

    Initial commit

---
+++ Behat/Context/DebugContext.php
@@ -0,0 +1,265 @@
+<?php
+
+/**
+ * @file
+ *
+ * Debug Context for Behat.
+ *
+ */
+
+namespace Metadrop\Behat\Context;
+
+
+use Behat\Behat\Context\SnippetAcceptingContext;
+use Drupal\DrupalExtension\Context\RawDrupalContext;
+
+
+use Behat\Behat\Hook\Scope\AfterScenarioScope;
+use Behat\Behat\Hook\Scope\AfterStepScope;
+use Behat\Testwork\Tester\Result\TestResult;
+
+use Behat\Gherkin\Node\FeatureNode;
+use Behat\Gherkin\Node\StepNode;
+
+/**
+ *
+ * Context to debug tests.
+ *
+ * This  context can save an error report on failed steps that includes:
+ *   - A text file with Behat error string and exception dump.
+ *   - A HTML dump of the current page.
+ *   - If driver is Selenium 2, a screenshot of the current page state.
+ *
+ * It also provides steps to generate screenshots and save page contents to
+ * file.
+ *
+ * Context params:
+ *   'report_on_error': If yes, the context generates the report on failed
+ *      steps.
+ *   'error_reporting_path': Path where to store generated reports.
+ *   'screenshots_path': Path where to store generated screenshots.
+ *   'page_contents_path': Path where to store page contents.
+ *
+ *  screenshots_path is used when asked to save a screenshot, while
+ *  error_reporting_path is used for error reporting. Thus, when a error report
+ *  includes a screenshot it's saved int he error_reporting_path path, along
+ *  the other files of the report.
+ *
+ */
+class DebugContext extends RawDrupalContext implements SnippetAcceptingContext {
+
+  const DEFAULT_HEIGHT = 600;
+
+  /**
+   * Context parameters.
+   *
+   * @var array
+   */
+  protected $customParams;
+
+  /**
+   * The Mink session.
+   *
+   * @var type Behat\Mink\Session
+   */
+  protected $session;
+
+  /**
+   * Step result
+   *
+   * @var type
+   */
+  protected $result;
+
+  /**
+   * Failed step.
+   *
+   * @var Behat\Gherkin\Node\StepNode
+   */
+  protected $step;
+
+  /**
+   * Failed step's feature.
+   *
+   * @var Behat\Gherkin\Node\FeatureNode
+   */
+  protected $feature;
+
+  /**
+   * Constructor.
+   *
+   * Save class params, if any.
+   *
+   * @param array $parameters
+   */
+  public function __construct($parameters) {
+
+    // Default values.
+    $defaults = array(
+      'report_on_error' => FALSE,
+      'error_reporting_path' => '/tmp',
+      'screenshots_path' => '/tmp',
+      'page_contents_path' => '/tmp',
+    );
+
+    // Collect received parameters.
+    $this->customParameters = array();
+    if (!empty($parameters)) {
+      // Filter any invalid parameters.
+      $this->customParameters = array_intersect_key($parameters, $defaults);
+    }
+
+    // Apply default values.
+    $this->customParameters += $defaults;
+  }
+
+  /**
+   * Is the reporting enabled?
+   *
+   * @return bool
+   *   Return TRUE if reporting is enabled.
+   */
+  public function isReportingEnabled() {
+    return $this->customParameters['report_on_error'];
+  }
+
+  /**
+   * Returns the configured path where to store error reports.
+   *
+   * @return string
+   *   Path where to store error reports.
+   */
+  public function getReportPath() {
+    return $this->customParameters['error_reporting_path'];
+  }
+
+  /**
+   * Returns the configured path where to store screenshots.
+   *
+   * @return string
+   *   Path where to store screenshots.
+   */
+  public function getScreenshotsPath() {
+    return $this->customParameters['screenshots_path'];
+  }
+
+  /**
+   * Returns the configured path where to store page contents files.
+   *
+   * @return string
+   *   Path where to store page contents files.
+   */
+  public function getPageContentPath() {
+    return $this->customParameters['page_contents_path'];
+  }
+
+  /**
+   * Returns the template for the files of the repor.
+   *
+   * All files form a report share the same name but extension is different.
+   *
+   * @return string
+   *   Filename template.
+   */
+  public function getFilenameReportTemplate() {
+    if (empty($this->filenameTemplate)) {
+      $this->filenameTemplate = 'Error-' . date("Ymd--H-i-s") . '_' . basename($this->feature->getFile(), '.feature') . '_line_' . $this->step->getLine();
+    }
+    return $this->filenameTemplate;
+  }
+
+  /**
+   * Generates and saves an error report.
+   *
+   * @param \FeatureNode $feature
+   *   Gherkin feature object.
+   * @param \StepNode $step
+   *   Gherkin step object.
+   */
+  public function saveReport() {
+    $this->session = $this->getSession();
+
+    $this->saveInfoFile();
+
+    // If it's Selenium Driver save a screenshot.
+    if ($this->session ->getDriver() instanceof \Behat\Mink\Driver\Selenium2Driver) {
+      $this->saveScreenshot($this->getFilenameReportTemplate() . '.png', $this->getReportPath());
+    }
+
+    // Dump HTML content.
+    $error_page_filepath = $this->getReportPath() . '/' . $this->getFilenameReportTemplate() . '.html';
+    file_put_contents($error_page_filepath, $this->session->getPage()->getContent());
+  }
+
+  /**
+   * Generates and saves the report info file.
+   *
+   * Contains the current URL and the error exception dump.
+   */
+  protected function saveInfoFile() {
+    $error_filepath = $this->getReportPath() . '/' . $this->getFilenameReportTemplate() . '.txt';
+
+    // Generate content.
+    $error_report = $this->session->getCurrentUrl() . "\n\n";
+    $error_report .= $this->result->getException();
+
+    // Save it.
+    file_put_contents($error_filepath, $error_report);
+  }
+
+  /**
+   * Generate a error report on failed step.
+   *
+   * @AfterStep
+   */
+  public function generateReportIfStepFailed(AfterStepScope $scope) {
+    if ($this->isReportingEnabled()) {
+      $this->result = $scope->getTestResult();
+      $test_failed = $this->result->getResultCode() === TestResult::FAILED;
+      if ($test_failed) {
+        $this->feature = $scope->getFeature();
+        $this->step = $scope->getStep();
+        $this->saveReport();
+      }
+    }
+  }
+
+  /**
+   * @Then capture full page with a width of :width
+   */
+  public function captureFullPageWithAWidthOf($width) {
+    $this->saveACaptureFullPageWithWidthOfTo($width, $this->getScreenshotsPath());
+  }
+
+  /**
+   * @Then save a capture full page with width of :width to :path
+   */
+  public function saveACaptureFullPageWithWidthOfTo($width, $path) {
+    // Use default height as screenshot is going to capture the complete page.
+    $this->getSession()->resizeWindow($width, $this::DEFAULT_HEIGHT, 'current');
+    $milliseconds = gettimeofday();
+    $filename = 'Screenshot-' . date("Ymd--H-i-s") . '.' . $milliseconds['usec'] . '.png';
+    $this->savescreenShot($filename, $path);
+  }
+
+  /**
+   * @Given /^save last response$/
+   *
+   * Step to save page content to a file.
+   */
+  public function saveLastResponse() {
+    $this->saveLastResponseToFile($this->getPageContentPath());
+  }
+
+  /**
+   * @Then save last response to :path
+   *
+   * Step to save page content fo a file in a given path.
+   */
+  public function saveLastResponseToFile($path) {
+    $milliseconds = gettimeofday();
+    $filename = 'PageContent-' . date("Ymd--H-i-s") . '.' . $milliseconds['usec'];
+    $error_page_filepath = $path . '/' . $filename . '.html';
+    file_put_contents($error_page_filepath, $this->getSession()->getPage()->getContent());
+  }
+}
---
+++ README.md
@@ -0,0 +1,76 @@
+# Behat Contexts
+
+
+Contexts that we use with Behat 3.x tests on Drupal sites.
+
+## Install
+
+This contexts use the Drupal Extension [Drupal Extension](https://www.drupal.org/project/drupalextension), so it should be installed in your system.
+
+Put this repository in the bootstrap directory of your project. A Drupal 7 project using Drupal Extension this directory may be located in sites/all/tests/behat/bootstrap. So, directory Metadrop of this repository should be in that bootstrap directory. Contexts will be autoladed.
+
+@TODO: Improve installation method, use some standard way.
+
+## Configure
+
+Each context may have its own configuration, see each context section.
+
+## Contexts
+
+### DebugContext
+
+Simple context to help debugging tests with some steps. Additionally, it hooks in the after step event to add a step that generates an error report on failed steps.
+
+This report includes:
+  - A file with the HTML page content.
+  - A file with the current URL and the error exception dump.
+  - If available, a file with current page state.
+
+
+#### Steps
+
+ - Then capture full page with a width of :width
+
+   Saves a screenshot of current page with the given width to a file.
+
+ - Then save a capture full page with width of :width to :path
+
+   Saves a screenshot of current page with the given width to a file in the given path.
+
+ - Then save last response
+
+   Saves page content to a file.
+
+ - Then save last response to :path
+
+   Saves page content to a file in the given path.
+
+#### Configuration
+  Add DebugContext to your suite
+
+  This is an example when bootstrap directorty is in DRUPALROOT/sites/all/tests/behat/bootstrap.
+
+```
+default:
+  autoload:
+    '': "%paths.base%/../all/tests/behat/bootstrap"
+  suites:
+    default:
+      paths:
+        - "%paths.base%/../all/tests/behat/features"
+      contexts:
+        - Metadrop\Behat\Context\DebugContext:
+            parameters:
+              'report_on_error': true
+              'error_reporting_path': '/var/error_reports/tests/reports'
+              'screenshots_path': '/var/error_reports/tests/screenshots'
+              'page_contents_path': '/var/error_reports/tests/pages'
+```
+
+**Parameters**
+  - report_on_error: If _true_ error reports are generated on failed steps.
+  - error_reporting_path: Path where reports are saved.
+  - screenshots_path: Path where screenshots are saved. Report screenshots are saved in the report path, here only screenshots from _capture full page_ steps are saved.
+  - page_contents_path: Path where page contents are saved. Report page contents are saved in the report path, here only page contents from _save page content_ steps are saved.
+
+
