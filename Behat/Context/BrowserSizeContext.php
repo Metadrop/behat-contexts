2f2f2d70cfea2ee8a4a8065fffe6350ce33f3de9
Ricardo Sanz <ricardo@metadrop.net>
2016-05-04 18:50:04 +0200

    Add docs, remove redundant step, improve paramters handling

--- Behat/Context/BrowserSizeContext.php
+++ Behat/Context/BrowserSizeContext.php
@@ -12,6 +12,17 @@ namespace Metadrop\Behat\Context;
 use Behat\Behat\Context\SnippetAcceptingContext;
 use Drupal\DrupalExtension\Context\RawDrupalContext;
 
+/**
+ * Context class to modify browser window size.
+ *
+ * This context allows easy browser window size change to a predefined sizes.
+ * This context needs a browser driver (Selenium2 for example).
+ *
+ * Context params:
+ *   'sizes': An array of predefined sizes. Each entry is a key-value pair with
+ *     the key eing the size name and the value and array with the width and
+ *     the height for taht size. @see __construct().
+ */
 class BrowserSizeContext extends RawDrupalContext implements SnippetAcceptingContext {
 
   /**
@@ -31,40 +42,34 @@ class BrowserSizeContext extends RawDrupalContext implements SnippetAcceptingCon
   public function __construct($parameters) {
 
     // Default values.
-    $defaults = array(
-      'Default'  => array('width' => 1200, 'height' => 800),
-      'Full'     => array('width' => 1200, 'height' => 800),
-      'Tablet H' => array('width' => 1024, 'height' => 768),
-      'Tablet V' => array('width' => 800, 'height' => 1024),
-      'Mobile H' => array('width' => 650, 'height' => 370),
-      'Mobile V' => array('width' => 350, 'height' => 650),
+    $this->customParameters = array(
+      'sizes' => array(
+        'Default'  => array('width' => 1200, 'height' => 800),
+        'Full'     => array('width' => 1200, 'height' => 800),
+        'Tablet H' => array('width' => 1024, 'height' => 768),
+        'Tablet V' => array('width' => 800, 'height' => 1024),
+        'Mobile H' => array('width' => 650, 'height' => 370),
+        'Mobile V' => array('width' => 350, 'height' => 650),
+      ),
     );
 
     // Collect received parameters.
-    $this->customParameters = array();
     if (!empty($parameters)) {
       // Filter any invalid parameters.
-      $this->customParameters = array_intersect_key($parameters, $defaults);
+      $parameters_filtered = array_intersect_key($parameters, $this->customParameters);
+
+      // Apply parameters.
+      $this->customParameters = array_replace_recursive($this->customParameters, $parameters_filtered);
     }
-
-    // Apply default values.
-    $this->customParameters += $defaults;
   }
 
   /**
-   * Step to resize the window by default before scenario.
+   * Step to resize the window to a given size.
+   *
    * This step is executed only if the stage has the tag @javascript.
-   * @BeforeScenario @javascript
-   */
-  public function browserWindowSizeIsDefault() {
-   $this->getSession()->resizeWindow($this->customParameters['Default']['width'], $this->customParameters['Default']['height'], 'current');
-   print_r("Browser Window Size: " . $this->customParameters['Default']['width'] . 'x' . $this->customParameters['Default']['height'] . ' px');
-  }
-
-  /**
-   * Step to resize the window with a given variable
+   *
    * @See $defaults
-   * This step is executed only if the stage has the tag @javascript.
+   *
    * @Given Browser window size is :arg1
    */
   public function browserWindowSizeIs($name) {
