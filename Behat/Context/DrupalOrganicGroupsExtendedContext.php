7547bb6a03831f715906567f9733da8d16574d6b
angel <angel.renedo@navandu.com>
2016-10-25 15:20:14 +0200

    Split og subscribe methods in 2 functions
    - Og subscribe method
    - Og subscribe with role method

--- Behat/Context/DrupalOrganicGroupsExtendedContext.php
+++ Behat/Context/DrupalOrganicGroupsExtendedContext.php
@@ -15,22 +15,16 @@ use Drupal\DrupalExtension\Context\RawDrupalContext;
 class DrupalOrganicGroupsExtendedContext extends RawDrupalContext implements SnippetAcceptingContext {
 
   /**
-   * Subscribe user to group with specific role.
-   * 
+   * Subscribe user to group.
+   *
    * @param string $group_type
    *   Entity type which user is subcript.
    * @param int $gid
-   *   Group id
-   * @param string $role
-   *   Role.
+   *   Group id.
    * @param object $user
-   *   User.
-   * 
+   *   User being subscript.
    */
-  public function assertUserBelongsToGroupWithRole($group_type, $gid, $role, $user)
-  {
-    $entities = entity_load($group_type, array($gid));
-    $entity = reset($entities);
+  public function subscribeUserToGroup($group_type, $gid, $user) {
     $membership = og_group($group_type, $gid, array(
       "entity type" => 'user',
       "entity" => $user,
@@ -39,10 +33,32 @@ class DrupalOrganicGroupsExtendedContext extends RawDrupalContext implements Sni
     if (!$membership) {
       throw new \Exception("The Organic Group membership could not be created.");
     }
+  }
+  
+  /**
+   * Subscribe user to group with specific role.
+   *
+   * @param string $group_type
+   *   Entity type which user is subcript.
+   * @param int $gid
+   *   Group id.
+   * @param string $role
+   *   Role.
+   * @param object $user
+   *   User being subscript.
+   */
+  public function subscribeUserToGroupWithRole($group_type, $gid, $role, $user)
+  {
+    // Subscript user to group.
+    $this->subscribeUserToGroup($group_type, $gid, $user);
+
+    // Load og entity.
+    $entities = entity_load($group_type, array($gid));
+    $entity = reset($entities);
 
     $og_roles = og_roles($group_type, $entity->type, $gid, FALSE, FALSE);
     $rid = array_search($role, $og_roles);
-    
+
     if (!$rid) {
       throw new \Exception("'$role' is not a valid group role.");
     }
