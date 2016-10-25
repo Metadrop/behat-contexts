ec76027d6bfaf8b16256289de67e1ed28bc8e727
angel <angel.renedo@navandu.com>
2016-10-25 15:10:50 +0200

    +new Context: organic groups

---
+++ Behat/Context/DrupalOrganicGroupsExtendedContext.php
@@ -0,0 +1,52 @@
+<?php
+
+/**
+ * @file
+ *
+ * DrupalUtilsContext Context for Behat.
+ *
+ */
+
+namespace Metadrop\Behat\Context;
+
+use Behat\Behat\Context\SnippetAcceptingContext;
+use Drupal\DrupalExtension\Context\RawDrupalContext;
+
+class DrupalOrganicGroupsExtendedContext extends RawDrupalContext implements SnippetAcceptingContext {
+
+  /**
+   * Subscribe user to group with specific role.
+   * 
+   * @param string $group_type
+   *   Entity type which user is subcript.
+   * @param int $gid
+   *   Group id
+   * @param string $role
+   *   Role.
+   * @param object $user
+   *   User.
+   * 
+   */
+  public function assertUserBelongsToGroupWithRole($group_type, $gid, $role, $user)
+  {
+    $entities = entity_load($group_type, array($gid));
+    $entity = reset($entities);
+    $membership = og_group($group_type, $gid, array(
+      "entity type" => 'user',
+      "entity" => $user,
+    ));
+
+    if (!$membership) {
+      throw new \Exception("The Organic Group membership could not be created.");
+    }
+
+    $og_roles = og_roles($group_type, $entity->type, $gid, FALSE, FALSE);
+    $rid = array_search($role, $og_roles);
+    
+    if (!$rid) {
+      throw new \Exception("'$role' is not a valid group role.");
+    }
+
+    og_role_grant($group_type, $gid, $user->uid, $rid);
+  }
+}
