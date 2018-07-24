d49dd334ed4220e2907b84ed14cea1d9990511bc
Eduardo <eduardo.morales@metadrop.net>
2018-07-24 12:24:49 +0200

    Created step check param with value in url.

--- Behat/Context/DrupalExtendedContext.php
+++ Behat/Context/DrupalExtendedContext.php
@@ -429,4 +429,30 @@ class DrupalExtendedContext extends RawDrupalContext implements SnippetAccepting
     $this->getSession()->wait($seconds * 1000, 'jQuery("#updateprogress").length === 0');
    }
 
+  /**
+   * Check param with value in url.
+   *
+   * @Then current url should have the ":param" param with ":value" value
+   */
+  public function urlShouldHaveParamWithValue($param, $value, $have = TRUE) {
+    $url = $this->getSession()->getCurrentUrl();
+    $queries = [];
+    parse_str(parse_url($url, PHP_URL_QUERY), $queries);
+    if (!(isset($queries[$param]) && $queries[$param] == $value) && $have) {
+      throw new \Exception("The param " . $param . " with value " . $value . " is not in the url");
+    }
+    elseif (isset($queries[$param]) && $queries[$param] == $value && !$have) {
+      throw new \Exception("The param " . $param . " with value " . $value . " is in the url");
+    }
+  }
+
+  /**
+   * Check param with value in url not exists.
+   *
+   * @Then current url should not have the ":param" param with ":value" value
+   */
+  public function urlShouldNotHaveParamWithValue($param, $value) {
+    $this->urlShouldHaveParamWithValue($param, $value, FALSE);
+  }
+
 }
