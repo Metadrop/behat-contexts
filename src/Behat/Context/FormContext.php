d0615d26786118a34e08f574a0a1576050afae6d
Omar Mohamad - El Hassan Lopesino <omar.lopesino@metadrop.net>
2018-12-17 17:58:37 +0100

    Context to manage forms

---
+++ src/Behat/Context/FormContext.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace Metadrop\Behat\Context;
+
+use NuvoleWeb\Drupal\DrupalExtension\Context\RawMinkContext;
+
+class FormContext extends RawMinkContext {
+
+  /**
+   * Gets info about required state of a form element.
+   *
+   * It relies on the requeried class added to he element by Drupal. This
+   * approach doesn't work with file type input elements.
+   *
+   * @param string $label
+   *   Form element label.
+   * @param string $type
+   *   Form element type.
+   * @throws \InvalidArgumentException
+   */
+  protected function isFormElementRequired($type, $label) {
+    if ($label === 'file') {
+      throw new \InvalidArgumentException("Form element \"file\" type not supported");
+    }
+
+    $page = $this->getSession()->getPage();
+
+    // Try to find element.
+    $xpath_element = "//label[contains(text(), '{$label}')]/..//{$type}";
+    $element = $page->find('xpath', $xpath_element);
+    if (NULL === $element) {
+      throw new \InvalidArgumentException("Could not find the form element \"$label\" of type \"$type\"");
+    }
+
+    // Check required class.
+    $xpath_required = "//label[contains(text(), '{$label}')]/..//{$type}[contains(@class, 'required')]";
+    $element_required = $page->find('xpath', $xpath_required);
+
+    return NULL !== $element_required;
+  }
+
+  /**
+   * Checks if a form element is required.
+   *
+   *
+   * @Then form :type element :label should be required
+   */
+  public function formElementShouldBeRequired($type, $label) {
+    if (!$this->isFormElementRequired($type, $label)) {
+      throw new \InvalidArgumentException("Form element \"$label\" of type \"$type\" is not required");
+    }
+  }
+
+  /**
+   * Checks if a form element is not required.
+   *
+   * @Then form :type element :label should not be required
+   */
+  public function formElementShouldNotBeRequired($type, $label) {
+    if ($this->isFormElementRequired($type, $label)) {
+      throw new \InvalidArgumentException("Form element \"$label\" of type \"$type\" is required");
+    }
+  }
+
+}
