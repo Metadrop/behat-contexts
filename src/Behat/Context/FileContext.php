e84aac39d0d69de83425c14ae8435b5767cbb61e
keopx <keopx@keopx.net>
2019-10-02 16:18:48 +0200

    Gets dinamically file path using private or public directory.
    Created new method to get public or private file path indendent of manually setup.

--- src/Behat/Context/FileContext.php
+++ src/Behat/Context/FileContext.php
@@ -46,4 +46,40 @@ class FileContext extends RawDrupalContext {
     $this->files[] = $this->getCore()->createFileWithName($path, $directory);
   }
 
+  /**
+   * Get path for file in drupal 8.
+   *
+   * @param string $filename
+   *   The name of the file to get.
+   * @param string directory
+   *   A string containing the files scheme, usually "public://".
+   *
+   * @throws Exception
+   *   Exception file not found.
+   *
+   * @throws Exception
+   *   Exception file could not be copied.
+   *
+   * @Given file get name :filename
+   * @Given file get name :filename in the :directory directory
+   */
+  public function visitFileWithName($filename, $directory = NULL) {
+    $public = 'public://';
+    $private = 'private://';
+
+    if (empty($directory) || $directory === $public) {
+      $realpath = \Drupal::service('file_system')->realpath($directory);
+      $path = str_replace(DRUPAL_ROOT, '', $realpath);
+      $destination = $path . '/' . basename($filename);
+    }
+
+    if (!empty($directory) && $directory === $private) {
+      $destination = \Drupal\Core\Url::fromRoute('system.private_file_download', ['filepath' => $filename], [
+        'relative' => TRUE,
+      ])->toString();
+    }
+
+    $this->visitPath($destination);
+  }
+  
 }
