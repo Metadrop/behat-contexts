528d1b42e89bcf306e33e55660ddb20e1b1fe365
Ricardo Sanz <ricardo@metadrop.net>
2021-10-27 09:37:39 +0100

    Revamp: improve comments, use a helper function

--- src/Behat/Context/UsersRandomContext.php
+++ src/Behat/Context/UsersRandomContext.php
@@ -5,29 +5,48 @@ namespace Metadrop\Behat\Context;
 use Behat\Gherkin\Node\TableNode;
 use Drupal\DrupalExtension\Context\DrupalContext;
 
+/**
+ * Context used to generate random user data.
+ *
+ * This context doesn't create any user, it creates random user data that
+ * can be used during tests.
+ * 
+ * For example, if you need to fill a form with name, email and password you  
+ * can use this context. Typically, this is used to interact with remote APIs 
+ * through a form. You could use fixed values, but if the API requires 
+ * different values on each test run (because there's no way to clean previous
+ * data created after submissions) this context can save your day.
+ * 
+ * @package Metadrop\Behat\Context
+ */
 class UsersRandomContext extends DrupalContext {
 
   /**
-   * Random users generated.
+   * Generated random user data.
    *
    * @var array
    */
   protected $randomUsers = [];
 
   /**
-   * Generate random users.
+   * Generate random user data.
+   *
+   * This step creates random user data (name, email and password) so it can be
+   * used on later steps.
+   *
    *
    * Do not use spaces or special characters.
    * Example:
-   * Given random users:
-   * | name        |
+   * Given random users identified by:
+   * | identifier  |
    * | debug       |
    * | email_test2 |
    *
    * @param \Behat\Gherkin\Node\TableNode $user_names_table
-   *   Names to identify random users.
+   *   Strings to identify generated random user data. Mainly used to identify
+   *   the user data on other steps.
    *
-   * @Given random users:
+   * @Given random users identified by:
    */
   public function generateRandomUsers(TableNode $user_names_table) {
     $hash = $user_names_table->getHash();
@@ -46,48 +65,72 @@ class UsersRandomContext extends DrupalContext {
   }
 
   /**
-   * Step for random user emails.
-   *
-   * @Then I fill in :mail with random email :random_mail_name
-   */
-  public function iFillInWithRandomMail($field, $random_mail_name) {
-    if (isset($this->randomUsers[$random_mail_name])) {
-      $random_mail = $this->randomUsers[$random_mail_name];
-      $this->minkContext->fillField($field, $random_mail['email']);
+   * Helper function to fill a form field with a given random user data.
+   * 
+   * @param $field
+   *   Field that would be populated with the requested random user data. The 
+   *   user  must have been generated previously.
+   * @param $random_user_identifier
+   *   Random generated user identifier.
+   * @param $data_name 
+   *   User data to put in the form. It can be 'username', 'email' or 'password'.
+   */ 
+  protected function fillFormFieldWithRandomUserData($field, $random_user_identifier, $data_name) {
+    if (isset($this->randomUsers[$random_user_identifier])) {
+      $random_user_data = $this->randomUsers[$random_user_identifier];
+      $this->minkContext->fillField($field, $random_user_data[$data_name]);
     }
     else {
-      throw new \InvalidArgumentException('There does not exists a random user with that name');
+      throw new \InvalidArgumentException("There does not exists a random user with the identifier '$random_user_identifier'");
     }
+  }  
+  
+  /**
+   * Step to fill a field with a previously generated random email.
+   *
+   * The random user data must have been generated previously.
+   *
+   * @param $field
+   *   Field that would be populated with the random user data mail. The user 
+   *   must have been generated previously.
+   * @param $random_user_identifier
+   *   Random generated user identifier.
+   *
+   * @Then I fill in :mail_field with random email from :random_user_identifier
+   */
+  public function iFillInWithRandomMail($field, $random_user_identifier) {
+    $this->fillFormFieldWithRandomUserData($field, $random_user_identifier, 'email');
   }
 
   /**
-   * Step for random user usernames.
+   * Step to fill a field with a previously generated random username.
    *
-   * @Then I fill in :field with random username :random_mail_name
+   * The random user data must have been generated previously.
+   *
+   * @param $field
+   *   Field that would be populated with the random user data username.
+   * @param $random_user_identifier
+   *   Random generated user identifier.
+   *
+   * @Then I fill in :field with random username from :random_user_identifier
    */
-  public function iFillInWithRandomUserName($field, $random_mail_name) {
-    if (isset($this->randomUsers[$random_mail_name])) {
-      $random_mail = $this->randomUsers[$random_mail_name];
-      $this->minkContext->fillField($field, $random_mail['username']);
-    }
-    else {
-      throw new \InvalidArgumentException('There does not exists a random user with that name');
-    }
+  public function iFillInWithRandomUserName($field, $random_user_identifier) {
+    $this->fillFormFieldWithRandomUserData($field, $random_user_identifier, 'username');
   }
 
   /**
-   * Step for random user password.
+   * Step to fill a field with a previously generated random password.
    *
-   * @Then I fill in :field with random password :random_mail_name
+   * The random user data must have been generated previously.
+   *
+   * @param $field
+   *   Field that would be populated with the random user data password.
+   * @param $random_user_identifier
+   *   Random generated user identifier.
+   *
+   * @Then I fill in :field with random password from :random_user_identifier
    */
-  public function iFillInWithRandomUserPassword($field, $random_mail_name) {
-    if (isset($this->randomUsers[$random_mail_name])) {
-      $random_mail = $this->randomUsers[$random_mail_name];
-      $this->minkContext->fillField($field, $random_mail['username']);
-    }
-    else {
-      throw new \InvalidArgumentException('There does not exists a random user with that name');
-    }
+  public function iFillInWithRandomUserPassword($field, $random_user_identifier) {
+    $this->fillFormFieldWithRandomUserData($field, $random_user_identifier, 'password');
   }
-
 }
