2c97efc8bbae4fe39fa42ab2bfa1c52d12a52482
Alejandro Cabarcos <alejandro.cabarcos@metadrop.net>
2023-09-27 08:36:55 +0200

    Issue #166: Specific access checking on getLastEntityId

--- src/Behat/Cores/Drupal8.php
+++ src/Behat/Cores/Drupal8.php
@@ -225,6 +225,7 @@ class Drupal8 extends OriginalDrupal8 implements CoreInterface {
     $query->sort($id_key, 'DESC');
     $query->range(0, 1);
     $query->addMetaData('account', \Drupal::entityTypeManager()->getStorage('user')->load(1));
+    $query->accessCheck(FALSE);
     $results = $query->execute();
 
     if (!empty($results)) {
