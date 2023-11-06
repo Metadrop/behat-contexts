80ac4f857d9c44d14c53287ffce1a27b640c3bbf
Juanjo <juanjol@users.noreply.github.com>
2023-11-06 13:53:48 +0100

    Update src/Behat/Cores/Drupal8.php
    Co-authored-by: Omar Mohamad - El Hassan Lopesino <omarmoper@gmail.com>

--- src/Behat/Cores/Drupal8.php
+++ src/Behat/Cores/Drupal8.php
@@ -235,7 +235,7 @@ class Drupal8 extends OriginalDrupal8 implements CoreInterface {
       case 'edit':
       case 'delete':
       case 'add':
-        $rel .= '-form';
+        $rel = sprintf('%s-form', $route) ;
         break;
       default:
         return $entity->toUrl()->getInternalPath() . '/' . $route;
