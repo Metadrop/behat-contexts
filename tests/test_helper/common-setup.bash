7f4316056e2d01af187aff28033588d57644b7a2
Ricardo Sanz <ricardo@metadrop.net>
2025-11-14 08:04:49 +0100

    For some reason mktemp doesn't like the quotes here

--- tests/test_helper/common-setup.bash
+++ tests/test_helper/common-setup.bash
@@ -25,7 +25,8 @@ export DDEV_BEHAT_DIR="/var/www/html"
 #
 setup_test_environment() {
   # Create unique temp directory for this test
-  TEST_TEMP_DIR=$(mktemp -d "${TMPDIR:-/tmp}/bats-behat-XXXXXX")
+  TEST_TEMP_DIR=$(mktemp -d ${TMPDIR:-~/tmp}/bats-behat-XXXXXX)
+
   cd "$TEST_TEMP_DIR"
 
 
